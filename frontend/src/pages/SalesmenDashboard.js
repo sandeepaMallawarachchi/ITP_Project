d3d67718c9907ef548e4b1f8697048cae14b4fce
IT22552242 <IT22552242@my.sliit.lk>
2024-02-06 23:16:02 +0530

    dashboard

--- frontend/src/pages/SalesmenDashboard.js
+++ frontend/src/pages/SalesmenDashboard.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import '../css/salesmenDashboard.css';
 import profilePic from '../images/Profile.png';
+
 
 export default function SalesmenDashboard() {
     const { id } = useParams();
     const [salesman, setSalesman] = useState({
         name: "",
         username: "",
-        dateOfBirth: "",
-        email: "",
-        phone: "",
-        address: "",
     });
     const navigate = useNavigate();
 
@@ -21,8 +19,8 @@ export default function SalesmenDashboard() {
                 const res = await axios.get(`http://localhost:5000/salesmen/salesmenDashboard/${id}`);
                 console.log(res.data);
                 const salesmanData = res.data.salesman || res.data;
-                const { name, username, dateOfBirth, email, phone, address } = salesmanData;
-                setSalesman({ name, username, dateOfBirth, email, phone, address });
+                const { name, username } = salesmanData;
+                setSalesman({ name, username });
             } catch (error) {
                 console.log("error", error.message);
             }
@@ -48,35 +46,36 @@ export default function SalesmenDashboard() {
 
     return (
         <div>
-            <div>
-                <p style={{ margin: "100px 0 0 1050px", fontWeight: "600" }}>Hello, <span style={{ color: "green" }}>{salesman.name}</span></p>
-            </div>
-            <div class="row" style={{ maxWidth: "550px", margin: "0 0 0 50px", textAlign: "left" }}>
-                <div class="col-4">
-                    <div class="list-group" id="list-tab" role="tablist">
-                        <a class="list-group-item list-group-item-action active" id="list-myacc-list" data-bs-toggle="list" href="#" role="tab" aria-controls="list-myacc">My Account</a>
-                        <a class="list-group-item list-group-item-action" id="list-newSale-list" data-bs-toggle="list" href="#list-newSale" role="tab" aria-controls="list-newSale">New Sale</a>
-                        <a class="list-group-item list-group-item-action" id="list-stocks-list" data-bs-toggle="list" href="#list-stocks" role="tab" aria-controls="list-stocks">Stocks</a>
-                        <a class="list-group-item list-group-item-action" id="list-salesHistory-list" data-bs-toggle="list" href="#list-salesHistory" role="tab" aria-controls="list-salesHistory">Sales History</a>
-                        <a class="list-group-item list-group-item-action" id="list-payment-list" data-bs-toggle="list" href="#list-payment" role="tab" aria-controls="list-payment">Payment</a>
-                        <a class="list-group-item list-group-item-action" id="list-Locations-list" data-bs-toggle="list" href="#list-Locations" role="tab" aria-controls="list-Locations">Locations</a>
-
-                    </div>
-                </div>
-            </div>
-            <div style={{ position: "absolute", padding: "20px", margin: "-280px 0 0 400px", textAlign: "left", fontWeight: "600" }}>
-                <img src={profilePic} style={{ maxHeight: "150px" }} />
-                <p>Name: <span style={{ color: "green" }}>{salesman.name}</span></p>
-                <p>Username: <span style={{ color: "green" }}>{salesman.username}</span></p>
-                <p>Date of Birth: <span style={{ color: "green" }}>{salesman.dateOfBirth}</span></p>
-                <p>Phone Number: <span style={{ color: "green" }}>{salesman.phone}</span></p>
-                <p>Address: <span style={{ color: "green" }}>{salesman.address}</span></p>
+            <div id='navigation'>
+                <p>LOGO</p>
+                <p>Hello, <span id='span'>{salesman.name}</span></p>
+                <a href='#'>My account</a>
+                <a href='#'>New sale</a>
+                <a href='#'>Sales history</a>
+                <a href='#'>Payment</a>
+                <a href='#'>Locations</a>
             </div>
 
-            <div>
+            <div id='profDetails'>
+                <img src={profilePic} id='profPic' />
+                <p>Name: <span id='span'>{salesman.name}</span></p>
+                <p>Username: <span id='span'>{salesman.username}</span></p>
+            </div>
+
+            <div id='profBtns'>
                 <button type="button" class="btn btn-primary" onClick={() => changePwBtn(id)}>Change Password</button><br />
                 <button type="button" class="btn btn-success" onClick={() => updateBtn(id)}>Update Account</button><br />
                 <button type="button" class="btn btn-danger" onClick={() => deleteBtn(id)}>Delete Account</button><br />
+            </div>
+
+            <div id='gmap'>
+                <p>Where am I?</p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63371.81536311016!2d79.81500561820582!3d6.9218368778345845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae253d10f7a7003%3A0x320b2e4d32d3838d!2sColombo!5e0!3m2!1sen!2slk!4v1707219695211!5m2!1sen!2slk"></iframe>
+            </div>
+
+            <div id='calander'>
+                <p>Calander</p>
+                <iframe src="https://calendar.google.com/calendar/embed?height=300&wkst=2&bgcolor=%2333B679&ctz=Asia%2FColombo&showTitle=0&showPrint=0&showTabs=0&showTz=0&showCalendars=0&src=ZW4ubGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%230B8043"></iframe>
             </div>
         </div>
     );
