e955793b27b6b7a3fedbfd431649fa566f96875f
kavindya-pabasara <it22219466@my.sliit.lk>
2024-03-18 12:32:34 +0530

    fixErrors

--- frontend/src/components/DeleteEmployee.js
+++ frontend/src/components/DeleteEmployee.js
@@ -4,23 +4,25 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 
 function DeleteEmployee() {
 
+    const { id } = useParams();
+    const navigate = useNavigate();
+
     const yesBtn = async () => {
         try {
             await axios.delete(`http://localhost:8070/staff/delete/${id}`);
-            //navigate("/dashboard");
+            navigate("/allEmployees");
         } catch (error) {
             console.error("Error deleting employee", error.message);
             alert("Error deleting employee. Please try again.");
         }
     };
-
     return (
         <div>
 
             <p>Are you sure you want to delete this employee?</p>
 
             <button style={{ margin: "0 30px 0 0" }} className='btn btn-danger' onClick={yesBtn}>Yes</button>
-            <Link to="/"><button className='btn btn-primary'>No</button></Link>
+            <Link to="/allEmployees"><button className='btn btn-primary'>No</button></Link>
 
         </div>
 
--- frontend/src/components/UpdateEmployee.js
+++ frontend/src/components/UpdateEmployee.js
@@ -13,7 +13,7 @@ function UpdateEmployee() {
         designation: "",
         address: "",
         email: "",
-        phoneNo: ""
+        phoneNo: "",
     });
     //const navigate = useNavigate();
 
@@ -21,7 +21,7 @@ function UpdateEmployee() {
         const fetchEmpDetails = async () => {
             try {
                 const response = await axios.get(`http://localhost:8070/staff/get/${id}`);
-                    console.log("Response data:", response.data);
+                console.log("Response data:", response.data);
 
                 const empData = response.data.employee || response.data;
                 const { empId, firstName, lastName, gender, department, designation, address, email, phoneNo } = empData;
@@ -37,108 +37,113 @@ function UpdateEmployee() {
         e.preventDefault();
 
         try {
-            await axios.put(`http://localhost:8070/staff/update/${id}`,empDetails);
-                alert("Details Updated!");
-                //navigate("/dashboard");
+            await axios.put(`http://localhost:8070/staff/update/${id}`, empDetails);
+            alert("Details Updated!");
+            //navigate("/dashboard");
         } catch (error) {
             console.error("Error updating employee details", error.message);
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div class="mb-3">
-                <label for="empId" class="form-label">Employee ID</label>
-                <input type="text" class="form-control" id="empId" value={empDetails.empId} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        empId: e.target.value,
-                    });
-                }}/>
-            </div>
-            
-            <div class="mb-3">
-                <label for="firstName" class="form-label">Employee First Name</label>
-                <input type="text" class="form-control" id="firstName" value={empDetails.firstName} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        firstName: e.target.value,
-                    });
-                }}/>
-            </div>
 
-            <div class="mb-3">
-                <label for="lastName" class="form-label">Employee Last Name</label>
-                <input type="text" class="form-control" id="lastName" value={empDetails.lastName} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        lastName: e.target.value,
-                    });
-                }}/>
-            </div>
+        <div className="container" style={{ width: "600px" }}>
+            <h1>hi, {empDetails.firstName}</h1>
+            <form onSubmit={handleSubmit}>
+                <div class="mb-3">
+                    <label for="empId" class="form-label">Employee ID</label>
+                    <input type="text" class="form-control" id="empId" value={empDetails.empId} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            empId: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <div class="mb-3">
-                <label for="gender" class="form-label">Employee Gender</label>
-                <input type="text" class="form-control" id="gender" value={empDetails.gender} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        gender: e.target.value,
-                    });
-                }}/>
-            </div>
+                <div class="mb-3">
+                    <label for="firstName" class="form-label">Employee First Name</label>
+                    <input type="text" class="form-control" id="firstName" value={empDetails.firstName} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            firstName: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <div class="mb-3">
-                <label for="department" class="form-label">Employee Department</label>
-                <input type="text" class="form-control" id="department" value={empDetails.department} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        department: e.target.value,
-                    });
-                }}/>
-            </div>
+                <div class="mb-3">
+                    <label for="lastName" class="form-label">Employee Last Name</label>
+                    <input type="text" class="form-control" id="lastName" value={empDetails.lastName} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            lastName: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <div class="mb-3">
-                <label for="designation" class="form-label">Employee Designation</label>
-                <input type="text" class="form-control" id="designation" value={empDetails.designation} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        designation: e.target.value,
-                    });
-                }}/>
-            </div>
+                <div class="mb-3">
+                    <label for="gender" class="form-label">Employee Gender</label>
+                    <input type="text" class="form-control" id="gender" value={empDetails.gender} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            gender: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <div class="mb-3">
-                <label for="address" class="form-label">Employee Address</label>
-                <input type="text" class="form-control" id="address" value={empDetails.address} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        address: e.target.value,
-                    });
-                }}/>
-            </div>
+                <div class="mb-3">
+                    <label for="department" class="form-label">Employee Department</label>
+                    <input type="text" class="form-control" id="department" value={empDetails.department} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            department: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <div class="mb-3">
-                <label for="email" class="form-label">Employee Email</label>
-                <input type="text" class="form-control" id="email" value={empDetails.email} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        email: e.target.value,
-                    });
-                }}/>
-            </div>
+                <div class="mb-3">
+                    <label for="designation" class="form-label">Employee Designation</label>
+                    <input type="text" class="form-control" id="designation" value={empDetails.designation} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            designation: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <div class="mb-3">
-                <label for="phoneNo" class="form-label">Employee Phone No</label>
-                <input type="text" class="form-control" id="phoneNo" value={empDetails.phoneNo} onChange={(e)=>{
-                    setEmpDetails({
-                        ...empDetails,
-                        phoneNo: e.target.value,
-                    });
-                }}/>
-            </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Employee Address</label>
+                    <input type="text" class="form-control" id="address" value={empDetails.address} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            address: e.target.value,
+                        });
+                    }} />
+                </div>
 
-            <button type="submit" class="btn btn-primary">Update</button>
-        </form>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Employee Email</label>
+                    <input type="text" class="form-control" id="email" value={empDetails.email} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            email: e.target.value,
+                        });
+                    }} />
+                </div>
+
+                <div class="mb-3">
+                    <label for="phoneNo" class="form-label">Employee Phone No</label>
+                    <input type="text" class="form-control" id="phoneNo" value={empDetails.phoneNo} onChange={(e) => {
+                        setEmpDetails({
+                            ...empDetails,
+                            phoneNo: e.target.value,
+                        });
+                    }} />
+                </div>
+
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+
+        </div>
     );
 
 }
