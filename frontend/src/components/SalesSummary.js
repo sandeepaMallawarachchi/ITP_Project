021e382937baa5193425205d479ca6f3532cd8cf
IT22552242 <sandeepamallawarachchi444@gmail.com>
2024-03-20 18:26:01 +0530

    add sale and sales summary

--- frontend/src/components/SalesSummary.js
+++ frontend/src/components/SalesSummary.js
@@ -33,13 +33,13 @@ function SalesSummary() {
 
     return (
         <div className='absolute mt-48 left-1/3 w-1/2 '>
-            <div class="mb-6">
+            <div className="mb-6">
                 <label for="cusID" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter Customer ID</label>
                 <input
-                    type="password"
+                    type="text"
                     id="cusID"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"                    placeholder="Enter customer ID"
-                    required
+                    placeholder="c123"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"                    required
                     onChange={(e) => {
 
                         setCusID(e.target.value);
@@ -48,10 +48,10 @@ function SalesSummary() {
             </div>
 
             <form>
-                <div class="mb-6">
+                <div className="mb-6">
                     <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current date</label>
                     <input
-                        type="password"
+                        type="text"
                         id="date"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required
@@ -59,10 +59,10 @@ function SalesSummary() {
                         value={salesSummary.date}
                     />
                 </div>
-                <div class="mb-6">
+                <div className="mb-6">
                     <label for="total" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total price</label>
                     <input
-                        type="password"
+                        type="text"
                         id="total"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required
@@ -72,7 +72,7 @@ function SalesSummary() {
                 </div>
 
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-14">
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-500">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" className="px-6 py-3">
