3518f6bac97d23523451bd90984660bfb43d3206
IT22552242 <sandeepamallawarachchi444@gmail.com>
2024-04-18 10:41:53 +0530

    sales manager dashboard updated

--- frontend/src/pages/salesManager/SalesManagerNavigations.js
+++ frontend/src/pages/salesManager/SalesManagerNavigations.js
@@ -94,7 +94,7 @@ export default function SalesManagerNavigations() {
     };
 
     const handleDashboard = () => {
-        navigate(`/salesManagerDashboard`);
+        navigate(`/salesManagerDashboard/${id}`);
     };
 
     const handleMyAccount = () => {
