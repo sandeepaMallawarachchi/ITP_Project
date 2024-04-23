d4cd5b73de51fe9306f449665f1b5fc4d67ed8ba
IT22552242 <sandeepamallawarachchi444@gmail.com>
2024-04-24 01:34:21 +0530

    financial

--- frontend/src/pages/financialManager/financialManagerLayouts.js
+++ frontend/src/pages/financialManager/financialManagerLayouts.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import FinancialManagerNavigations from "./financialManagerNavigations";
+import FinancialManagerNavigations from "./FinancialManagerNavigations";
 
 export default function FinancialManagerLayout() {
 
