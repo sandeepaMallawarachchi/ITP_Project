4da9645b9aad62c6c5e668566d5646074508da38
kavindya-pabasara <it22219466@my.sliit.lk>
2024-04-25 00:33:32 +0530

    Add css

--- frontend/src/pages/staffManager/AddEmployee.js
+++ frontend/src/pages/staffManager/AddEmployee.js
@@ -110,9 +110,9 @@ function AddEmployee() {
                     }} />
                 </div>
 
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Employee</button>
             </form>
-            <button type="submit" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" onClick={handleAllEmployees}>All Employees</button>
+
         </div>
     )
 }
