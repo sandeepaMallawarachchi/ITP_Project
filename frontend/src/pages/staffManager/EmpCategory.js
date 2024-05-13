6535da652ba7d86709c733302258a0c825d13365
kavindya-pabasara <it22219466@my.sliit.lk>
2024-05-13 17:19:30 +0530

    FixErrors

--- frontend/src/pages/staffManager/EmpCategory.js
+++ frontend/src/pages/staffManager/EmpCategory.js
@@ -1,45 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, useParams } from 'react-router-dom';
 
 function EmpCategory() {
 
     const navigate = useNavigate();
+    const {id} = useParams();
 
     const handleAddEmployee = () => {
-        navigate(`/staff/EmpCategory/${id}`);
+        navigate(`/staff/addEmployee/${id}`);
     };
 
-    const handleAddSalesPerson = () => {
-        navigate(`/staff/EmpCategory/${id}`);
-    };
+    // const handleAddSalesPerson = () => {
+    //     navigate(`/staff/EmpCategory/${id}`);
+    // };
 
     const handleAddDriver = () => {
-        navigate(`/staff/EmpCategory/${id}`);
+        navigate(`/deliveryManager/addDrivers/${id}`);
     };
 
     return (
         <div className='absolute mt-48 left-1/3 w-1/2'>
-            <Link to="/http://localhost:8070/staff/add/${id}">
                 <div>
                     <img src="managers.jpg" alt="managers" />
-                    <p>Managers</p>
+                    <button>Managers</button>
                 </div>
-            </Link>
 
-            <Link to="/http://localhost:8070/salesmen/addSalesmen/${id}">
                 <div>
                     <img src="salesPersons.jpg" alt="salesPersons" />
-                    <p>Sales Persons</p>
+                    <button>Sales Persons</button>
                 </div>
-            </Link>
 
-            <Link to="/http://localhost:8070/driver/add/${id}">
                 <div>
                     <img src="drivers.jpg" alt="drivers" />
-                    <p>Drivers</p>
+                    <button>Drivers</button>
                 </div>
-            </Link>
 
         </div>
     );
