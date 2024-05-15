94c099faaa21a1438c8cc33d0c88ef43781b4a41
kavindya-pabasara <it22219466@my.sliit.lk>
2024-05-15 17:49:26 +0530

    top seller

--- frontend/src/pages/staffManager/TopSellers.js
+++ frontend/src/pages/staffManager/TopSellers.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import topOneImg from '../../images/topOne.jpg'
+import topTwoImg from '../../images/topTwo.jpg'
+import topThreeImg from '../../images/topThree.jpg'
 
 function TopSellers() {
     const { id } = useParams();
@@ -21,8 +24,10 @@ function TopSellers() {
     return (
         <div className='absolute mt-48 left-72 w-3/4 flex justify-center'>
             {topSellers.map((seller, index) => (
-                <div key={index} className='w-1/3 mr-7'>
-                    <img src={`seller${index + 1}Img.jpg`} alt={`seller${index + 1}`} />
+                <div key={index} className='w-1/3 mr-7 flex flex-col items-center'>
+                    {index === 0 && <img src={topOneImg} alt={`seller${index + 1}`} className='w-36'/>}
+                    {index === 1 && <img src={topTwoImg} alt={`seller${index + 1}`} className='w-36'/>}
+                    {index === 2 && <img src={topThreeImg} alt={`seller${index + 1}`} className='w-36'/>}
                     <p className='text-center'>{`Top Seller ${index + 1}`}</p>
                     <p className='text-center'>{`Seller ID: ${seller._id}`}</p>
                     <p className='text-center'>{`Seller name: ${names[index]}`}</p> {/* Render seller name */}
