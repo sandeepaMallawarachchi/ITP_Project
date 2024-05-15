3e65c285dcab363dc47e86dc1f486e7d9314a6e1
kavindya-pabasara <it22219466@my.sliit.lk>
2024-05-16 02:39:26 +0530

    fix

--- frontend/src/pages/staffManager/AddSalary.js
+++ frontend/src/pages/staffManager/AddSalary.js
@@ -11,7 +11,7 @@ function AddSalary() {
     const [basicSalary, setBasicSalary] = useState(0);
     const [ETFbonus, setETFbonus] = useState(0);
     const [EPFbonus, setEPFbonus] = useState(0);
-    const [year, setYear] = useState(new Date().getFullYear().toString());
+    const [paymentDate, setPaymentDate] = useState("");
     const [empIdError, setEmpIdError] = useState("");
 
     const { id } = useParams();
@@ -134,7 +134,7 @@ function AddSalary() {
 
                 <div className="mb-3">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="paymentDate">Payment Date: </label>
-                    <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="date" id="paymentDate" value={paymentDate} readOnly />
+                    <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="date" id="paymentDate" value={paymentDate} onChange={(e) => setPaymentDate(e.target.value)} />
                 </div>
 
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Salary</button>
