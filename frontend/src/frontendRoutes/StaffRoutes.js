51fae4d382658e5c386f2c5feb9a057d7f5bb2f6
IT22552242 <sandeepamallawarachchi444@gmail.com>
2024-05-16 02:20:01 +0530

    driver update delete

--- frontend/src/frontendRoutes/StaffRoutes.js
+++ frontend/src/frontendRoutes/StaffRoutes.js
@@ -44,7 +44,7 @@ export default function StaffRoutes() {
                 <Route path='/addDriver/:id' element={<AddDriver />} />
                 <Route path='/updateSalesperson/:salesPersonId/:id' element={<UpdateSalesPerson />} />
                 <Route path='/deleteSalesperson/:salespersonID/:id' element={<DeleteSalesPerson />} />
-                <Route path='/deleteDriver/:driverId/:id' element={<DeleteDriver />} />
+                <Route path='/deleteDriver/:dID/:id' element={<DeleteDriver />} />
                 <Route path='/topSellers/:id' element={<TopSellers />} />
                 <Route path='/updateDriver/:dID/:id' element={<UpdateDriver />} />
             </Route>
--- frontend/src/pages/staffManager/DeleteDriver.js
+++ frontend/src/pages/staffManager/DeleteDriver.js
@@ -5,7 +5,7 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 function DeleteDriver() {
     const { id, dID } = useParams();
     const navigate = useNavigate();
- 
+
     const yesBtn = async () => {
         try {
             await axios.delete(`http://localhost:8070/empLogin/deleteDriver/${dID}`);
@@ -25,9 +25,9 @@ function DeleteDriver() {
         <div className='absolute mt-48 left-1/3 w-1/2'>
             <p>Are you sure you want to delete this driver?</p>
 
-             <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' onClick={yesBtn}>Yes</button>
-             <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={noBtn}>No</button>
-             
+            <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' onClick={yesBtn}>Yes</button>
+            <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={noBtn}>No</button>
+
         </div>
     );
 }
